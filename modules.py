715cf76336400c2fff9a00c7cd559bc1ed85aaef
AndreFGard <andrefsagh@gmail.com>
2023-12-25 09:17:36 -0300

    create GameResult object and makeInlineQueryResultArticle func

--- modules.py
+++ modules.py
@@ -1,4 +1,8 @@
 import json
+import gazpacho
+from telegram import InlineQueryResultArticle, InputTextMessageContent
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton
+from uuid import uuid4
 
 class cachev0:
     def __init__(self, jsonFileName: str):
@@ -19,3 +23,42 @@ def digitsToEmoji(digit: str):
 
 def discountToEmoji(discount: str):
     return digitsToEmoji(discount[1:-1])
+
+
+
+class GameResult:
+    def __init__(self, tag: list, cacheStorage):
+        self.link = tag.attrs["href"]
+        self.title = tag.text
+        #if appid is not in there, it's a bundle.
+        self.appid = tag.attrs["data-ds-appid"] if "data-ds-appid" in tag.attrs else tag.attrs["data-ds-bundleid"]
+        self.itad_plain = cacheStorage[self.appid] if self.appid in cacheStorage else "not found"
+
+        self.pricetag = tag.find("div", {"class": "col search_price_discount_combined responsive_secondrow"}, mode="first")
+        self.price = int(self.pricetag.attrs["data-price-final"]) * 0.01
+        self.discount = self.pricetag.text if self.pricetag.text.startswith("-") else ""
+
+def makeInlineQueryResultArticle(result: GameResult):
+    return InlineQueryResultArticle(
+                id=uuid4(),
+                title=result.title,
+                hide_url=True,
+                description=f"Price: {result.price:.2f}" + (f"   [{result.discount}]" if result.discount else  ""),
+                thumb_url=f"https://cdn.akamai.steamstatic.com/steam/apps/{result.appid}/capsule_sm_120.jpg?t",  #low qual thumb
+                # description=description,
+                input_message_content=InputTextMessageContent(
+                    parse_mode="Markdown",
+                    message_text=f"[{result.title}]({result.link})\nPrice: R$ {result.price:.2f}" + (f"\nDiscount: -{discountToEmoji(result.discount)}%" if result.discount else ""), #https://cdn.akamai.steamstatic.com/steam/apps/{appid}/header.jpg? can be used in order to not show the game's description
+                ),
+                reply_markup=InlineKeyboardMarkup(
+                    (
+                       (
+                            InlineKeyboardButton("Steam Page", url=result.link),
+                            InlineKeyboardButton("ProtonDB 🐧",url=f"https://www.protondb.com/app/{result.appid}"),
+                        ),
+                        [InlineKeyboardButton("Historico de preços", url=f"https://isthereanydeal.com/game/{result.itad_plain}/info/")], #creates a button in its own line, bellow the buttons above
+                    )
+                ),
+            )
+        
+#results = list(map(makeInlineQueryResultArticle))
