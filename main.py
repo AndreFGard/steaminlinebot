a50f4f0f5430008778994386165b9f0edb8b2ef4
AndreFGard <96950645+AndreFGard@users.noreply.github.com>
2026-01-12 18:59:45 -0300

    refactor: clean main.py and adapt to new structure

--- main.py
+++ main.py
@@ -11,10 +11,12 @@ Inspired by the archewikibot
 @Steaminlinebot written by GuaximFsg (now AndreFGard) on github
 """
 
+from functools import cache
 from math import trunc
 import os
 import sys
 import logging
+from typing import Callable
 from uuid import uuid4
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton, Update
 from telegram.ext import Updater, InlineQueryHandler, CommandHandler
@@ -22,23 +24,14 @@ from telegram import InlineQueryResultArticle, InputTextMessageContent
 import json
 import modules
 import time
+from telegram.ext import Application
 
-MAX_RESULTS = 6
+from modules.GameResult import GameResult
+from modules.TelegramQueryMaker import TelegramInlineQueryMaker, ERROR_RESULT, TOO_SHORT_RESULT, NO_MATCHES_RESULT
+from modules.SteamSearcher import SteamSearcher
+
 import dotenv
 dotenv.load_dotenv()
-
-itad_key = ''
-
-
-ERROR_RESULT = modules.ERROR_RESULT 
-
-# Enable logging
-# logging.basicConfig(
-#     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-#     level=logging.INFO,
-# )
-
-# logger = logging.getLogger(__name__)
 
 
 async def start(update: Update, context):
@@ -57,39 +50,55 @@ async def help(update: Update, context):
     )
 
 
+class Bot:
+    def __init__(self):
+        self.queryMaker = TelegramInlineQueryMaker(SteamSearcher(MAX_RESULTS=6))
+
+    @staticmethod
+    def sortGameResults(a):
+        errors = [ERROR_RESULT, TOO_SHORT_RESULT, NO_MATCHES_RESULT]
+        return list(filter(lambda x: x not in errors, a)) + list(filter(lambda x: x in a, errors))
+
+    async def handleInlineQuery(self, update: Update, context):
+        query = update.inline_query.query #type:ignore
+        start = time.time()
+        
+        specialResults = set()
+        results =[]
+        if len(query) < 3:
+                specialResults.add(TOO_SHORT_RESULT)
+        else:
+            try:
+                res = await self.queryMaker.scrapeQuery(query)
+                gameResults = res.results
+
+                if not gameResults:
+                    specialResults.add(NO_MATCHES_RESULT)
+                
+                if res.found_error:
+                    specialResults.add(ERROR_RESULT) #type:ignore
+                                
+                #telegram Inline Objects that are actually rendered
+                results: list[InlineQueryResultArticle] = []
+                for r in gameResults:
+                    try:
+                        results.append(self.queryMaker.makeInlineQueryResultArticle(r))
+                    except Exception as e:
+                        print(f"LOG: ERROR: {e}")      
+                        specialResults.add(ERROR_RESULT)
+            
+            except Exception as e:
+                print(f"LOG: Failed to query: {e}")
+                results.add(ERROR_RESULT)  #type:ignore
+
+        updateTime = time.time()
+        await update.inline_query.answer(results + list(specialResults), cache_time=30) #type:ignore
+        endTime = time.time()
+        print(f"LOG: scrape time: {(updateTime - start):.4f}s, totalTime: {(endTime - start):.4f}s")
+                
 
 
-steamSearcher = modules.SteamSearcher(MAX_RESULTS)
-async def inlinequerySteamApi(update: Update, context):
-    start = time.time()
-    query = update.inline_query.query #type:ignore
-    if len(query) < 3:
-        return
-    
-    try:
-        gameResults = await steamSearcher.makeGameResultsFromGameDetails(query)
-        if len(gameResults) == 0:
-            gameResults = [ERROR_RESULT]
-    except:
-        gameResults = [ERROR_RESULT]
-
-
-    telegramResultArticles =  list(map(modules.makeInlineQueryResultArticle, gameResults))
-    for idx,game in enumerate(telegramResultArticles):
-        if not game:
-            #move errored result to the end of the list
-            telegramResultArticles.pop(idx)
-            telegramResultArticles.append(modules.ERROR_RESULT)
-    results = list(filter(lambda x: isinstance(x, InlineQueryResultArticle), telegramResultArticles))
-    start_uploading = time.time()
-    await update.inline_query.answer(results, cache_time=30)
-    end = time.time()
-    print(f"took {(start_uploading - start):.4f}s + {(end - start_uploading):.4f}s")
-
-#choose which version to use here
-INLINEQUERYFUNC = inlinequerySteamApi
-
-def error(update, context):
+def error(update: Update, context):
     print(f"Update {update} caused error {context.error}")
 
 
@@ -101,8 +110,9 @@ def main():
         print("No BOT_TOKEN environment variable passed. Terminating.")
         sys.exit(1)
     
-    from telegram.ext import Application
-    
+    bot = Bot()
+
+
     # Create the Application
     application = Application.builder().token(token).build()
 
@@ -111,13 +121,16 @@ def main():
     application.add_handler(CommandHandler("help", help))
 
     # on noncommand i.e message - echo the message on Telegram
-    application.add_handler(InlineQueryHandler(INLINEQUERYFUNC))
+    application.add_handler(InlineQueryHandler(bot.handleInlineQuery))
 
     # log all errors
     application.add_error_handler(error)
-
+    
     # Start the Bot
     application.run_polling()
+
+
+
 
 
 if __name__ == "__main__":
