001d624dbb151aebd0177708cb1aaae025bb2c86
AndreFGard <96950645+AndreFGard@users.noreply.github.com>
2026-01-28 11:38:24 -0300

    feat: create search games service

---
+++ modules/services/SearchGames.py
@@ -0,0 +1,98 @@
+import logging
+from sqlite3 import Connection
+
+from dataclasses import dataclass
+import time
+
+from modules.SteamSearcher import SteamSearcher
+from modules.db.UserRepository import UserRepository
+from modules.db.GameResultRepository import GameResultRepository
+from typing import Optional
+from pydantic import BaseModel
+from modules.ProtonDBReport import ProtonDBReport, ProtonDBTier
+from modules.services.UserCountry import UserCountry
+@dataclass
+class ProtonDBVM:
+    tier: ProtonDBTier
+    positive_trend: bool
+    totalReports: int
+
+@dataclass
+class GameResultVM:
+    """View Model"""
+    id: int
+    link: str
+    title: str
+    appid: str
+    price: Optional[str]
+    is_free: bool
+    discount: Optional[str]
+    protonDB: Optional[ProtonDBVM]
+
+from enum import Enum
+
+class SpecialResults(Enum):
+    NO_MATCHES = 1
+    ERROR = 2
+    CONFIGURE_COUNTRY =3
+    QUERY_TOO_SHORT = 4
+
+@dataclass
+class SearchResults:
+    results: list[GameResultVM]
+    specialResults: list[SpecialResults]
+    scrapeTime: float
+
+class SearchGames:
+    def __init__(self, db:Connection):
+        self._db = db
+        self._userRepo = UserRepository(db)
+        self._gameResultRepo = GameResultRepository(db)
+        self._searcher = SteamSearcher(MAX_RESULTS=6)
+        self._userCountry = UserCountry(db)
+    async def searchGame(self, userId, query, fallback_languages=[]):
+        errors = set()
+        results = []
+
+        if len(query) < 3:
+            errors.add(SpecialResults.QUERY_TOO_SHORT)
+            return SearchResults(results, list(errors), 0.0)
+        
+        if not fallback_languages: fallback_languages.append("US")
+        country_configured,country = self._userCountry.get_country(userId,fallback_languages)
+
+        start = time.time()
+        res = await self._searcher.scrapeGameResults(query, country)
+        end = time.time()
+
+        if res.found_error:
+            errors.add(SpecialResults.ERROR)
+        if not res.results:
+            errors.add(SpecialResults.NO_MATCHES)
+        if not country_configured:
+            errors.add(SpecialResults.CONFIGURE_COUNTRY)
+        
+
+        for r in res.results:
+            try:
+                rId = self._gameResultRepo.insert_game_result(r)
+                protonDBVm = (ProtonDBVM(
+                    tier=r.protonDBReport.tier,
+                    positive_trend=r.protonDBReport.trendingTier > r.protonDBReport.tier,
+                    totalReports=r.protonDBReport.total)
+                        if r.protonDBReport else None)
+                results.append(GameResultVM(
+                    id=rId,
+                    link=r.link,
+                    title=r.title,
+                    appid=r.appid,
+                    price=r.price,
+                    is_free=r.is_free,
+                    discount=r.discount,
+                    protonDB=protonDBVm
+                ))
+            except Exception as e:
+                logging.info(f"Error at searchGame when building Result: {e}")
+                errors.add(SpecialResults.ERROR)
+
+        return SearchResults(results, list(errors), end-start)
---
+++ modules/services/UserCountry.py
@@ -0,0 +1,39 @@
+import logging
+from sqlite3 import Connection
+
+from dataclasses import dataclass
+import time
+
+from telegram import Update
+
+from modules.SteamSearcher import SteamSearcher
+from modules.db.UserRepository import UserRepository
+from modules.db.GameResultRepository import GameResultRepository
+from typing import Optional
+from pydantic import BaseModel
+from modules.ProtonDBReport import ProtonDBReport, ProtonDBTier
+
+
+class UserCountry:
+    def __init__(self, db:Connection):
+        self._userRepo = UserRepository(db)
+
+
+    def get_country(self, userId:int, fallback_languages=[]):
+        country = self._userRepo.get_user_country(userId)
+        has_set=True
+        if not country:
+            has_set = False
+            for l in fallback_languages:
+                country = self._userRepo.get_country_by_language(l)
+                if country: break
+
+        if not country: country = "US"
+        return (has_set,country )
+
+    async def setCountry(self, userId: int, country:str):
+        country = country.upper()
+        try:
+            success = self._userRepo.upsert_user_country(userId, country)
+        except:
+            success = False
